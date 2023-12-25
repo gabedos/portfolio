e60dd0bafa4a8346cddada876b350532223d05d5
gabedos <gabe.dos212@gmail.com>
2023-12-25 16:17:24 -0500

    Prettierrc formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,60 @@
 import React from "react";
 // import { Link } from "react-scroll";
-import logo from '../assets/react.svg'
+import logo from "../assets/react.svg";
 import Hamburger from "./Hamburger";
 
-export default function Navbar() {
+export default function Navbar(props) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
 
   function toggleNavbar() {
-    setNavbarOpen(prevNavbarOpen => !prevNavbarOpen);
-    console.log("toggled")
+    setNavbarOpen((prevNavbarOpen) => !prevNavbarOpen);
   }
 
-  console.log("re-rendering")
-
   return (
-    <nav className="z-[100] p-5 bg-white shadow md:flex md:items-center md:justify-between max-h-[80px]">
-
-      <div className="flex justify-between items-center content-center">
-        <span className="text-2xl font-[Poppins] cursor-pointer">
-          <img className="h-10 inline"
-            src={logo} />
+    <nav
+      className={`fixed top-0 z-[100] max-h-[${props.navBarHeight}px] w-[100%] bg-slate-100 p-5 shadow-lg md:flex md:items-center md:justify-between`}
+    >
+      <div className="flex content-center items-center justify-between">
+        <span className="cursor-pointer font-[Poppins] text-2xl">
+          <img className="inline h-10" src={logo} />
           <span className="ml-4">gabedos</span>
         </span>
 
-        <span className="text-3xl cursor-pointer mx-2 md:hidden block">
+        <span className="mx-2 block cursor-pointer text-3xl md:hidden">
           <Hamburger navbarOpen={navbarOpen} toggleNavbar={toggleNavbar} />
         </span>
       </div>
 
-      <ul className={`${navbarOpen ? "top-[80px] opacity-100" : ""} text-center md:flex md:items-center z-[50] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto p-0 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500`}>
+      <ul
+        className={`${
+          navbarOpen ? `top-[${props.navBarHeight}px] opacity-100` : ""
+        } absolute left-0 top-[-400px] z-[50] w-full bg-slate-100 p-0 text-center opacity-0 transition-all duration-500 ease-in md:static md:z-auto md:flex md:w-auto md:items-center md:opacity-100`}
+      >
         <li className="mx-4 my-6 md:my-0">
-          <a href="#" className="text-xl hover:text-cyan-500 duration-500">Home</a>
+          <a href="#" className="text-xl duration-500 hover:text-cyan-500">
+            Home
+          </a>
         </li>
         <li className="mx-4 my-6 md:my-0">
-          <a href="#" className="text-xl hover:text-cyan-500 duration-500">Portfolio</a>
+          <a href="#" className="text-xl duration-500 hover:text-cyan-500">
+            Portfolio
+          </a>
         </li>
         <li className="mx-4 my-6 md:my-0">
-          <a href="#" className="text-xl hover:text-cyan-500 duration-500">About</a>
+          <a href="#" className="text-xl duration-500 hover:text-cyan-500">
+            About
+          </a>
         </li>
         <li className="mx-4 my-6 md:my-0">
-          <a href="#" className="text-xl hover:text-cyan-500 duration-500">Skills</a>
+          <a href="#" className="text-xl duration-500 hover:text-cyan-500">
+            Skills
+          </a>
         </li>
 
-        <button className="bg-cyan-400 text-white font-[Poppins] duration-500 px-6 py-2 mx-4 md:mb-0 mb-6 hover:bg-cyan-500 rounded transition-none">
+        <button className="mx-4 mb-6 rounded bg-cyan-400 px-6 py-2 font-[Poppins] text-white transition-none duration-500 hover:bg-cyan-500 md:mb-0">
           Contact
         </button>
       </ul>
-
     </nav>
-  )
+  );
 }
